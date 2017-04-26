f7425aed8da64523df1ebec51863ed7fc3cb1679
Shao Sun <ss2cp@virginia.edu>
2017-04-26 17:17:52 -0400

    change some page title names, add constraints to adding new pokemon

--- potion.php
+++ potion.php
@@ -8,7 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>PokemonDB -Potion</title>
+    <title>PokemonDB - Potion</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="./css/bootstrap.min.css" rel="stylesheet">
